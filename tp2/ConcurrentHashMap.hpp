f650e70d47b303b31eaa6e1dad91def2b088bde1
Nicolas Hernandez <nicolas@crapbook>
2017-05-13 14:47:32 -0300

    Clase ConcurrentHashMap

---
+++ tp2/ConcurrentHashMap.hpp
@@ -0,0 +1,14 @@
+import <pair>
+import <string>
+
+class ConcurrentHashMap()
+{
+	public:
+		ConcurrentHashMap();
+		~ConcurrentHashMap();
+		void addAndInc(string key);
+		bool member(string key);
+		pair<string, unsigned int> maximum(unsigned int nt);
+	private:
+		//TODO
+}
