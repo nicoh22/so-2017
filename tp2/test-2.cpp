3e2cd8db4acc2fc9a7a1964e33df7c530cc3bb30
Esteban <estebanlucianorey@gmail.com>
2017-05-28 17:35:55 -0300

    descomento test 2

--- tp2/test-2.cpp
+++ tp2/test-2.cpp
@@ -7,12 +7,12 @@ int main(void) {
 	ConcurrentHashMap h;
 	int i;
 	h = ConcurrentHashMap::count_words("corpus");
-	/*for (i = 0; i < 26; i++) {
+	for (i = 0; i < 26; i++) {
 		for (auto it = h.tabla[i].CrearIt(); it.HaySiguiente(); it.Avanzar()) {
 			auto t = it.Siguiente();
 			cout << t.first << " " << t.second << endl;
 		}
-	}*/
+	}
 	return 0;
 }
 
